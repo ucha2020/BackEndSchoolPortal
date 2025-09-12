b116faa8b0434af26b2876b37d6b220827c096fa
ucha <uchaokwudili@gmail.com>
2025-09-12 13:18:59 -0700

    ok

--- App/config/model_sync.js
+++ App/config/model_sync.js
@@ -1,76 +1,76 @@
-const sequelize = require('../config/database');
-const Course = require('../model-s/course');
-const Lecturer = require('../model-s/lecturer');
-const Faculty = require('../model-s/faculty');
-const Department = require('../model-s/department');
-const Student = require('../model-s/student');
-const { DataTypes } = require('sequelize');
+const sequelize = require("../config/database");
+const Course = require("../model-s/course");
+const Lecturer = require("../model-s/lecturer");
+const Faculty = require("../model-s/faculty");
+const Department = require("../model-s/department");
+const Student = require("../model-s/student");
+const { DataTypes } = require("sequelize");
 
-const Student_Course = sequelize.define('Student_Course',{
-    studentId: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Student,
-            key: 'id',
-        }
+const Student_Course = sequelize.define("Student_Course", {
+  studentId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Student,
+      key: "id",
     },
-    courseId: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Course,
-            key: 'id',
-        }
+  },
+  courseId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Course,
+      key: "id",
     },
+  },
 });
 
-Faculty.hasMany(Department,{
-    foreignKey:{
-        allowNull: false,    
-    },
-    onDelete: 'RESTRICT',
+Faculty.hasMany(Department, {
+  foreignKey: {
+    allowNull: false,
+  },
+  onDelete: "RESTRICT",
 });
 Department.belongsTo(Faculty);
 
-Department.hasMany(Lecturer,{
-    foreignKey:{
-        allowNull: true,
-    },
-    onDelete: 'RESTRICT',
+Department.hasMany(Lecturer, {
+  foreignKey: {
+    allowNull: true,
+  },
+  onDelete: "RESTRICT",
 });
 Lecturer.belongsTo(Department);
 
-Department.hasMany(Student,{
-    foreignKey:{
-        allowNull: true,
-    },
-    onDelete: 'RESTRICT',
+Department.hasMany(Student, {
+  foreignKey: {
+    allowNull: false,
+  },
+  onDelete: "RESTRICT",
 });
 Student.belongsTo(Department);
 
-Lecturer.hasMany(Course,{
-    foreignKey:{
-        allowNull: false,
-    },
+Lecturer.hasMany(Course, {
+  foreignKey: {
+    allowNull: false,
+  },
 });
 Course.belongsTo(Lecturer);
 
-Lecturer.hasMany(Student,{
-    foreignKey:{
-        allowNull: true,
-    }
+Lecturer.hasMany(Student, {
+  foreignKey: {
+    allowNull: true,
+  },
 });
 Student.belongsTo(Lecturer);
 
-Student.belongsToMany(Course,{through:Student_Course});
-Course.belongsToMany(Student,{through:Student_Course});
+Student.belongsToMany(Course, { through: Student_Course });
+Course.belongsToMany(Student, { through: Student_Course });
 
-(async () => {
-    const fac = await Faculty.sync({force:true});
-    const dep = await Department.sync({force:true});
-    const lec = await Lecturer.sync({force:true});
-    const cou = await Course.sync({force:true});
-    const stu = await Student.sync({force:true});
-    const stu_cou = await Student_Course.sync({force:true});
-    const tst = 54;
-})();
-module.exports = {Lecturer,Course,Faculty,Department,Student};
+async () => {
+  const fac = await Faculty.sync({ force: true });
+  const dep = await Department.sync({ force: true });
+  const lec = await Lecturer.sync({ force: true });
+  const cou = await Course.sync({ force: true });
+  const stu = await Student.sync({ force: true });
+  const stu_cou = await Student_Course.sync({ force: true });
+  const tst = 54;
+};
+module.exports = { Lecturer, Course, Faculty, Department, Student };
--- App/routes/student.js
+++ App/routes/student.js
@@ -1,28 +1,30 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const student = require('../controllerss/studentController');
-const { fa } = require('@faker-js/faker');
+const student = require("../controllerss/studentController");
+const { fa } = require("@faker-js/faker");
 
-route.get('/:id/display',student.student_page);
+route.get("/", student.students_page);
 
-route.get('/',student.students_page);
+route.get("/create", student.student_creation_form);
 
-route.get('/create',student.student_creation_form);
+route.post("/create_edit", student.student_creation_form);
 
-route.post('/create_edit',student.student_creation_form);
+route.post("/create", student.student_create_formData_processor);
 
-route.post('/create',student.student_create_formData_processor);
+route.get("/:id/display", student.student_page);
 
-route.get("/:id/update",student.student_update_form);
+route.get("/:id/update", student.student_update_form);
 
-route.get("/:id/update_add",student.student_update_add_form);
+route.get("/:id/update_add", student.student_update_add_form);
 
-route.post("/:id/update",student.student_update_formData_processor);
+route.post("/:id/update_add", student.student_update_add_form);
 
-route.post("/:id/update_edit",student.student_update_form);
+route.post("/:id/update", student.student_update_formData_processor);
 
-route.get("/:id/delete",student.student_delete_form);
+route.post("/:id/update_edit", student.student_update_form);
 
-route.post("/:id/delete",student.student_delete_form);
+route.get("/:id/delete", student.student_delete_form);
+
+route.post("/:id/delete", student.student_delete_form);
 
 module.exports = route;
